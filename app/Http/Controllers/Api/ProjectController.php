b383cdc4af1b6150da11665176df5ca9dc1aaa3a
Salvatore <salvatorebono2001@gmail.com>
2023-10-16 16:00:57 +0200

    milestone 1 and 2 completed

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //with("type") = recupero i dati relativi a type
+        //paginate(12) = avrò una paginazione di 12 risultati per pagina
+        $projects = Project::with("type")->paginate(12);
+        //Risposta JSON contenente come dati $projects
+        return response()->json($projects);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
